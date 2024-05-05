8c1fd04aa13afc429dc2e76f1f050e8d3aef9e8a
UGenee <148860670+UGenee@users.noreply.github.com>
2024-05-05 21:05:28 +0800

    updates

--- app/src/main/java/com/bonifacio/baon/dashboard.java
+++ app/src/main/java/com/bonifacio/baon/dashboard.java
@@ -1,6 +1,11 @@
 package com.bonifacio.baon;
 
+import android.content.Context;
+import android.content.Intent;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +13,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import org.w3c.dom.Text;
+
 public class dashboard extends AppCompatActivity {
 
+
+    private android.widget.TextView TextView;
+    TextView balanceTextView = (TextView);
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_dashboard);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        balanceTextView = findViewById(R.id.balance);
+
+        Intent intent = getIntent();
+        String budgetInput = intent.getStringExtra("BUDGET_INPUT");
+
+        balanceTextView.setText(budgetInput);
     }
+
 }
